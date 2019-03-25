7576ee910d2c8be3d68a3653bde5cd53a4e09674
Barbara Fick <bkantzos@yahoo.com>
2019-03-25 12:56:47 -0600

    added post-list component and dummy post as property array.   loop over list for displa in the html.  also show a no posts yet if no posts.length is 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule, MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+} from '@angular/material';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +29,7 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
