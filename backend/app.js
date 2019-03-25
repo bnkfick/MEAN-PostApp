f8cfad58e00eee23e49e927ba73eb0d0bccc8abc
Barbara Fick <bkantzos@yahoo.com>
2019-03-25 15:54:40 -0600

    added node/express backend files that server up api/posts with dummy post json data

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fadf12421l",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "fasdfag234",
+      title: "Second server-side post",
+      content: "This is the Second post coming from the server!"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts
+  });
+});
+
+module.exports = app;
