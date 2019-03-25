195796249764ee6524ffabe3aeaebb7c8da0970c
Barbara Fick <bkantzos@yahoo.com>
2019-03-25 12:20:59 -0600

    added angular material toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     PostCreateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
