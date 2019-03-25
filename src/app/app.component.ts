25b5bdd4a3ee131fa0eb0ad3feb10aa5e4e3ea64
Barbara Fick <bkantzos@yahoo.com>
2019-03-25 13:11:05 -0600

    add post functionality with input, output eventemitter.  working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  storedPosts = [];
   title = 'mean-project';
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -7,11 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class PostCreateComponent {
-  newPost = 'NO CONTENT';
-  enteredValue = '';
-  onAddPost(postInput: HTMLTextAreaElement) {
-    console.dir();
-    this.newPost = this.enteredValue;
+
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter();
+
+  onAddPost() {
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 }
 /* template binding
