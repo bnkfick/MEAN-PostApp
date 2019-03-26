6f0483473e324961421559b2d98ac79ad9f04936
Barbara Fick <bkantzos@yahoo.com>
2019-03-25 23:54:07 -0600

    connecting to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const Post = require('./models/post');
 const app = express();
 
+mongoose.connect("mongodb+srv://bnkfick:ZjgK7Ow7bAAgFgA5@max-mean-oor1m.mongodb.net/test?retryWrites=true")
+.then(() => {
+  console.log('Connected to database!')
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
