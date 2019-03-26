4e952669e194cc165cf14f29d976e318b2356fb3
Barbara Fick <bkantzos@yahoo.com>
2019-03-26 16:56:47 -0600

    adding auth component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {
-  MatToolbarModule,
-  MatExpansionModule,
   MatInputModule,
   MatCardModule,
   MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
   MatProgressSpinnerModule,
-  MatPaginatorModule,
+  MatPaginatorModule
 } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,13 +20,13 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
-
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     PostCreateComponent,
+    HeaderComponent,
     PostListComponent,
     LoginComponent,
     SignupComponent
@@ -36,16 +37,18 @@ import { SignupComponent } from './auth/signup/signup.component';
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
     MatPaginatorModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
